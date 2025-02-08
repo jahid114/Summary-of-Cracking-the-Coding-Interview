5714d447da41bb6be227a3f0a092b8bbe43eec03
jahid <jahidctgcse@gmail.com>
2025-02-09 02:50:59 +0600

    Added solution of isUnique problem in chap -1

---
+++ Interview Questions/Chap -1 ( Arrays and String )/IsUnique.java
@@ -0,0 +1,21 @@
+// Implement and algorithm to determine if a string has all unique characters. What if you can't use additional data structure?
+
+// Assuming that the string is an ASCII string 
+
+class Main {
+  public static void main(String[] args) {
+      System.out.println(isUnique("Helo"));
+  }
+  
+  public static boolean isUnique( String s ){
+      boolean[] charExistance = new boolean[256];
+      int asciiValue;
+      for( char ch : s.toCharArray() ){
+          asciiValue = (int) ch;
+          if( charExistance[asciiValue] )
+              return false;
+          charExistance[asciiValue] = true;
+      }
+      return true;
+  }
+}
